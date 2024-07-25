8a2eeee5e334350176f69dc32cfd98efeade240e
Michael Nau <michaelwnau@gmail.com>
2024-07-25 13:39:07 -0500

    chore: Add Zeplin configuration files

--- with-storybook-app/generate-zeplin-config.js
+++ with-storybook-app/generate-zeplin-config.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 require('dotenv').config();
 
+console.log('ZEPLIN_PROJECT_ID:', process.env.ZEPLIN_PROJECT_ID);
+console.log('ZEPLIN_STYLEGUIDE_ID:', process.env.ZEPLIN_STYLEGUIDE_ID);
+
 const zeplinConfig = {
   projects: [process.env.ZEPLIN_PROJECT_ID],
   styleguides: [process.env.ZEPLIN_STYLEGUIDE_ID],
@@ -8,3 +11,4 @@ const zeplinConfig = {
 };
 
 fs.writeFileSync('zeplin.json', JSON.stringify(zeplinConfig, null, 2));
+console.log('zeplin.json has been created successfully.');
